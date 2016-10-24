cfd3aff4c14b86d340238ae7e105c12aad231f47
locomalo <elleandro@gmail.com>
2016-10-24 18:37:16 -0300

    minor fixes

--- kudubot/services/local_services/AppointmentService.py
+++ kudubot/services/local_services/AppointmentService.py
@@ -61,14 +61,14 @@ class AppointmentService(Service):
         if message.message_body.lower().split(" ", 2)[1] == 'nuevo':
             language, _, activity, dayMonthYear_Hour = message.message_body.lower().split(" ", 3)
             reply = self.createAppointment(activity,
-                                           datetimeConvert(dayMonthYear_Hour), address)
+                                           self.datetimeConvert(dayMonthYear_Hour), address)
         if message.message_body.lower().split(" ", 2)[1] == 'almacen':
             language, _, databaseNamea = message.message_body.lower().split(" ", 2)
-            reply = self.setup(activity, datetimeConvert(dayMonthYear_Hour), address)
+            reply = self.setup(activity, self.datetimeConvert(dayMonthYear_Hour), address)
 
         else:
             language, activity, dayMonthYear_Hour = message.message_body.lower().split(" ", 2)
-            reply = self.addActivity(activity, datetimeConvert(dayMonthYear_Hour), address)
+            reply = self.addActivity(activity, self.datetimeConvert(dayMonthYear_Hour), address)
         # TODO: Accept double spaces if present...
         # address in WA this is the
         #be telephoneNumber with the @s.whatsapp.net ...
