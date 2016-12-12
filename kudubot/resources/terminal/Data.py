baa35d48dbe44ded78b1751dbadaa9d4b039a45f
fernamar <fernandomarso@gmail.com>
2016-12-11 22:04:36 -0300

    Update Data.py

--- kudubot/resources/terminal/Data.py
+++ kudubot/resources/terminal/Data.py
@@ -440,4 +440,15 @@ SALIDA LLEGADA EMPRESA
 21:30 03:00    *TIGRE IGUAZU* _Cama_
 23:30 05:15    *FLECHA BUS* _Semi Cama_
 23:30 05:15    *FLECHA BUS* _Cama Ejecutivo_
-""", }
+""", '#parana-concordia': """SALIDA LLEGADA EMPRESA
+05:45	10:20	*FLECHA BUS*
+08:15	12:35	*FLECHA BUS*
+11:15	15:30	*ZENIT*
+13:30	17:30	*RAPIDO TATA*
+15:30	20:20	*ZENIT*
+15:35	19:40	*RAPIDO* _Solo Viernes_
+18:45	23:00	*FLECHA BUZ*
+18:55	23:00   *RAPIDO* _Solo Viernes_
+20:10	00:20	*RAPIDO TATA* _Excepto Sabado y Domingo_
+23:55	04:05	*FLECHA BUS* _Solo Lunes_
+""",}
