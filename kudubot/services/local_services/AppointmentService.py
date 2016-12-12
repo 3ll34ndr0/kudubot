925c93a7298be5e50fbaae6e3a3eccd7d2f72687
locomalo <elleandro@gmail.com>
2016-12-12 15:54:00 -0300

    minor improvement

--- kudubot/services/local_services/AppointmentService.py
+++ kudubot/services/local_services/AppointmentService.py
@@ -126,7 +126,7 @@ class AppointmentService(Service):
                 reply = ""
                 if creds is not None:
                     for c in creds:
-                        reply += "*{}*: {} vencen {}".format(c.activity,
+                        reply += "*{}*: {} vencen {}\n".format(c.activity,
                                                       c.credits,
                                                       c.expireDate.strftime("%d %h %Y"))
                 else:
